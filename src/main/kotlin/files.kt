410e4ee9bef02212ad948f97496e350887cca49e
Alex Lenkov <@>
2024-07-18 23:11:34 +0300

    KTB-04 solution

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -1,15 +1,29 @@
 import java.io.File
 
+data class Word(
+    val original : String,
+    val translate : String,
+    val correctAnswersCount : Int = 0,
+)
+
 fun main() {
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
+    val dictionary: MutableList<Word> = mutableListOf()
 
     for (string in wordsFile.readLines()) {
-        println(string)
+        val split = string.split("|")
+        try {
+            val word = Word(original = split[0],
+                translate = split[1],
+                correctAnswersCount = split[2].toInt())
+            dictionary.add(word)
+        }catch (e: Exception) {
+            val word = Word(original = split[0],
+                translate = split[1])
+            dictionary.add(word)
+        }
     }
 
+    dictionary.map { println(it) }
 
 }
