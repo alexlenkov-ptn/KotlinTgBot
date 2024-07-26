5bde74612920d0307ff6a18d99badedbd457596f
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-07-26 22:13:52 +0300

    KTB-06 added statistic

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.round
 
 data class Word(
     val original: String,
@@ -19,7 +20,47 @@ fun main() {
         )
         dictionary.add(word)
     }
+    while (true) {
+        println(
+            "Меню: \n" +
+                    "1 – Учить слова\n" +
+                    "2 – Статистика\n" +
+                    "0 – Выход"
+        )
+        val userInput = readln().toString()
+        when (userInput) {
+            "1" -> {
+                println("Учим слова")
+                break
+            }
 
-    dictionary.map { println(it) }
+            "2" -> {
+                println(
+                    "Смотрим статистику\n" +
+                            dictionary.filter()
+                )
+                break
+            }
 
+            "0" -> {
+                println("Выходим")
+                break
+            }
+
+            else -> {
+                println("Нажмите 1, 2 или 3")
+                continue
+            }
+        }
+    }
+}
+
+fun MutableList<Word>.filter(): String {
+    val allElements = this.count()
+    var correctAnswer = 0
+    this.map {
+        if (it.correctAnswersCount >= 3) correctAnswer++
+    }
+    val percentResult = ((correctAnswer.toDouble() / allElements.toDouble()) * 100).toInt()
+    return "Выучено $correctAnswer из $allElements слов | $percentResult%"
 }
