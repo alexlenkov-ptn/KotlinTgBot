d5b956fe010119797e902bc9c4bcf57fa7e05f4c
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-08-04 13:31:22 +0300

    KTB-07

--- src/main/kotlin/files.kt
+++ src/main/kotlin/files.kt
@@ -1,6 +1,10 @@
+@file:Suppress("UNUSED_CHANGED_VALUE")
+
 import java.io.File
 
 const val CORRECT_ANSWER = 3
+const val ANSWER_OPTIONS = 4
+const val ONE_WORD = 1
 
 data class Word(
     val original: String,
@@ -32,6 +36,7 @@ fun main() {
         when (userInput) {
             1 -> {
                 println("Учим слова")
+                dictionary.printWords()
                 break
             }
 
@@ -59,8 +64,31 @@ fun main() {
 fun MutableList<Word>.printStatistics(): String {
     val allElements = this.count()
 
-    val correctAnswer = this.filter { it.correctAnswersCount >= CORRECT_ANSWER }.count()
+    val correctAnswer = this.count { it.correctAnswersCount >= CORRECT_ANSWER }
 
     val percentResult = ((correctAnswer.toDouble() / allElements.toDouble()) * 100).toInt()
     return "Выучено $correctAnswer из $allElements слов | $percentResult%"
+}
+
+fun MutableList<Word>.printWords() {
+    while (true) {
+        val unlearnedWords = this.filter { it.correctAnswersCount < CORRECT_ANSWER }
+        val unlearnedWordsOptions = unlearnedWords.shuffled().take(ANSWER_OPTIONS)
+        var options = 1
+        if (unlearnedWords.isEmpty()) {
+            println("Все слова выучены")
+            break
+        } else {
+            println("Загадываемое слово ${unlearnedWordsOptions.take(ONE_WORD).map { it.original }}\n" +
+                    "Варианты ответа:")
+
+            unlearnedWordsOptions.shuffled().map {
+                println("$options. ${it.translate}")
+                options++
+            }
+
+            println("Напишите ответ: ")
+            val userInput = readln().toIntOrNull()
+        }
+    }
 }
