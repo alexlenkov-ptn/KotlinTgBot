6a4fe71eddf11d36442a6b4a60becea4caf67f2e
avlenkov <alex.lenkov@mail.ru>
2024-09-12 17:31:13 +0300

    KTB 14

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -14,11 +14,11 @@ fun main(args: Array<String>) {
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val updateIdRegex : Regex = "\"update_id\":(.+?),".toRegex()
+        val matchResult : MatchResult? = updateIdRegex.find(updates)
+        val groups = matchResult?.groups
+
+        updateId = (groups?.get(1)?.value?.toInt()?.plus(1)) ?: 0
     }
 }
 
